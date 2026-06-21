c856eb75ea0bf0ae669622911ffd8d0ee526ae5b
Senseipayto3 <115595966+Senseipayto3@users.noreply.github.com>
2026-06-21 16:58:39 -0500

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,10 @@
+import { render } from "../ui.js";
+
+export default async function () {
+    render(`
+        <h2>Analytics</h2>
+        <div class="card">
+            <p>Charts and graphs coming soon.</p>
+        </div>
+    `);
+}
